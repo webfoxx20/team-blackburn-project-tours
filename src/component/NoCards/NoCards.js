78b257307a87a5211463dcb31078a15ab6a22cff
Ayoola Oladipe <haywise20@gmail.com>
2021-12-01 11:39:47 +0100

    bugfix title

--- src/component/NoCards/NoCards.js
+++ src/component/NoCards/NoCards.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import "./NoCards.css"
+import React from "react";
+import "./NoCards.css";
 
 const NoCards = (props) => {
-    return (
-        <div className="container">
-            <h2 className="title">No More Tours Remaining</h2>
-            <button className="refreshBtn" onClick={() => window.location.reload()}>Refresh</button>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h2 className="no-card-title">No More Tours Remaining</h2>
+      <button className="refreshBtn" onClick={() => window.location.reload()}>
+        Refresh
+      </button>
+    </div>
+  );
+};
 
-export default NoCards
+export default NoCards;
